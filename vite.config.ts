38852e9301711b15e851e00586a58590855a65a5
sahoo-ak <aloka.ku.sahoo@gmail.com>
2025-03-22 18:22:06 +0100

    Force redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  base: './', // Add this line for GitHub Pages compatibility
+export default defineConfig({
+  base: '/cv-onepage/', // Ensure this is correct
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
